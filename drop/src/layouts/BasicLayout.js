592adde1dda0fc21fdafe0a3ed21c163552250bf
xujixin11 <18331290137@163.com>
2017-12-14 20:50:40 +0800

    落网

--- drop/src/layouts/BasicLayout.js
+++ drop/src/layouts/BasicLayout.js
@@ -1,115 +1,59 @@
 /**
  * 2017-12-12 Xu Jixin
  */
-import React from 'react';
-import { Container, Divider, Dropdown, Grid, Header, Image, List, Menu, Segment } from 'semantic-ui-react';
+import React, { Component } from 'react';
+import { Container, Segment, Input, Menu } from 'semantic-ui-react';
 import 'semantic-ui-css/semantic.min.css';
+import Slider from 'react-slick';
 
-const FixedMenuLayout = () => (
-  <div>
-    <Menu fixed="top" inverted>
-      <Container>
-        <Menu.Item as="a" header>
-          <Image
-            size="mini"
-            src="/logo.png"
-            style={{ marginRight: '1.5em' }}
-          />
-          Project Name
-        </Menu.Item>
-        <Menu.Item as="a">Home</Menu.Item>
 
-        <Dropdown item simple text="Dropdown">
-          <Dropdown.Menu>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-            <Dropdown.Divider />
-            <Dropdown.Header>Header Item</Dropdown.Header>
-            <Dropdown.Item>
-              <i className="dropdown icon" />
-              <span className="text">Submenu</span>
-              <Dropdown.Menu>
-                <Dropdown.Item>List Item</Dropdown.Item>
-                <Dropdown.Item>List Item</Dropdown.Item>
-              </Dropdown.Menu>
-            </Dropdown.Item>
-            <Dropdown.Item>List Item</Dropdown.Item>
-          </Dropdown.Menu>
-        </Dropdown>
-      </Container>
-    </Menu>
+class BasicLayout extends Component {
+  state = { activeItem: 'home' }
 
-    <Container text style={{ marginTop: '7em' }}>
-      <Header as="h1">Semantic UI React Fixed Template</Header>
-      <p>This is a basic fixed menu template using fixed size containers.</p>
-      <p>A text container is used for the main container, which is useful for single column.</p>
+  handleItemClick = (e, { name }) => this.setState({ activeItem: name })
 
-      <Image src="/assets/images/wireframe/media-paragraph.png" style={{ marginTop: '2em' }} />
-      <Image src="/assets/images/wireframe/paragraph.png" style={{ marginTop: '2em' }} />
-      <Image src="/assets/images/wireframe/paragraph.png" style={{ marginTop: '2em' }} />
-      <Image src="/assets/images/wireframe/paragraph.png" style={{ marginTop: '2em' }} />
-      <Image src="/assets/images/wireframe/paragraph.png" style={{ marginTop: '2em' }} />
-      <Image src="/assets/images/wireframe/paragraph.png" style={{ marginTop: '2em' }} />
-      <Image src="/assets/images/wireframe/paragraph.png" style={{ marginTop: '2em' }} />
-    </Container>
+  render() {
+    const { activeItem } = this.state;
+    const settings = {
+      dots: true,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 1,
+      slidesToScroll: 1,
+    };
+    return (
+      <div>
+        <Container style={{ background: 'whitesmoke', width: '100%' }}>
+          <Menu secondary style={{ justifyContent: 'center', height: '60', alignItems: 'center', borderTop: '2px solid red' }} >
+            <Menu.Item name="落." active={activeItem === 'periodical'} style={{ fontSize: '20' }} />
+            <Menu.Item name="期刊" active={activeItem === 'periodical'} onClick={this.handleItemClick} />
+            <Menu.Item name="单曲" active={activeItem === 'single'} onClick={this.handleItemClick} />
+            <Menu.Item name="专栏" active={activeItem === 'special'} onClick={this.handleItemClick} />
+            <Menu.Item name="活动" active={activeItem === 'activity'} onClick={this.handleItemClick} />
+            <Menu.Item name="APP" active={activeItem === 'app'} onClick={this.handleItemClick} />
+            <Menu.Menu>
+              <Menu.Item>
+                <Input icon="search" placeholder="Search..." />
+              </Menu.Item>
+              <Menu.Item name="注册/登录" active={activeItem === 'logout'} style={{ marginLeft: '150' }} onClick={this.handleItemClick} />
+            </Menu.Menu>
+          </Menu>
+        </Container>
 
-    <Segment
-      inverted
-      vertical
-      style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
-    >
-      <Container textAlign="center">
-        <Grid divided inverted stackable>
-          <Grid.Row>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Group 1" />
-              <List link inverted>
-                <List.Item as="a">Link One</List.Item>
-                <List.Item as="a">Link Two</List.Item>
-                <List.Item as="a">Link Three</List.Item>
-                <List.Item as="a">Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Group 2" />
-              <List link inverted>
-                <List.Item as="a">Link One</List.Item>
-                <List.Item as="a">Link Two</List.Item>
-                <List.Item as="a">Link Three</List.Item>
-                <List.Item as="a">Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Group 3" />
-              <List link inverted>
-                <List.Item as="a">Link One</List.Item>
-                <List.Item as="a">Link Two</List.Item>
-                <List.Item as="a">Link Three</List.Item>
-                <List.Item as="a">Link Four</List.Item>
-              </List>
-            </Grid.Column>
-            <Grid.Column width={3}>
-              <Header inverted as="h4" content="Footer Header" />
-              <p>Extra space for a call to action inside the footer that could help re-engag.</p>
-            </Grid.Column>
-          </Grid.Row>
-        </Grid>
+        <Container style={{ marginTop: '2em', width: '100%', paddingLeft: '200px' }}>
+          <Slider {...settings} style={{ textAlign: 'center' }}>
+            <div><img src="http://img-cdn2.luoo.net/site/201711/5a129472a4c9b.jpg" alt="小球：微醺一整片天空" draggable="false" /></div>
+            <div><img src="http://img-cdn2.luoo.net/site/201708/5993be0007772.jpg" alt="颜值爆表女主唱——她们不被外形与性别定义" draggable="false" /></div>
+            <div><img src="http://img-cdn2.luoo.net/site/201709/59b25ac16f984.jpg" alt="Pulp：关于生、死、超市" draggable="false" /></div>
+            <div><img src="http://img-cdn2.luoo.net/site/201711/59fc7102afee3.jpg" alt="谁是下一个地下丝绒" draggable="false" /></div>
+          </Slider>
+        </Container>
 
-        <Divider inverted section />
-        <Image
-          centered
-          size="mini"
-          src="/logo.png"
-        />
-        <List horizontal inverted divided link>
-          <List.Item as="a" href="#">Site Map</List.Item>
-          <List.Item as="a" href="#">Contact Us</List.Item>
-          <List.Item as="a" href="#">Terms and Conditions</List.Item>
-          <List.Item as="a" href="#">Privacy Policy</List.Item>
-        </List>
-      </Container>
-    </Segment>
-  </div>
-);
-
-export default FixedMenuLayout;
+        <Segment style={{ margin: '5em 0em 0em', background: 'white' }}>
+          <Segment>Footer</Segment>
+        </Segment>
+      </div>
+    );
+  }
+}
+export default BasicLayout;
