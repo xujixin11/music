42910e0b0ef38a2666423562e6edbc861b5ae02a
xujixin11 <18331290137@163.com>
2018-01-11 20:39:17 +0800

    落网

--- drop/src/routes/App.js
+++ drop/src/routes/App.js
@@ -8,9 +8,11 @@ import 'semantic-ui-css/semantic.min.css';
 class App extends Component {
   render() {
     return (
-      <Container style={{ width: '660px', margin: '0 auto', minHeight: '313px', marginTop: '85px' }}>
-        <div style={{ width: '215px', height: '447px' }}>
-          a
+      <Container style={{ width: '660px', margin: '0 auto', minHeight: '316px', marginTop: '85px' }}>
+        <div style={{ width: '216px', height: '448px', marginRight: '116px', background: 'require(../assets/iphone.jpg)' }}>
+          <div style={{ display: 'block', width: '190px', height: '338px', marginTop: '54px', marginLeft: '12px', overflow: 'hidden' }}>
+            <img src="http://img-cdn2.luoo.net/system/59520a3a407cf.jpg" alt="pic" style={{ height: '338px', marginLeft: 'auto' }} />
+          </div>
         </div>
       </Container>
     );
